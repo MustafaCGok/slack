403a0efe4ee7e237ba6190aef59ca47b0085bb3a
MustafaCGok <muccemil@gmail.com>
2020-05-13 17:00:08 +0200

    update CreateChannel.java

--- src/day_2/CreateChannel.java
+++ src/day_2/CreateChannel.java
@@ -3,10 +3,11 @@ package day_2;
 public class CreateChannel {
     public static void main(String[] args) {
 
-        System.out.println("Useris able to create channels");
-        int x = 0;
-        if (x == 0) {
-            System.out.println("equal");
+        String s= "a";
+        if(s.contains("a")){
+            System.out.println("contains");
+        }else{
+            System.out.println("not");
         }
 
     }
