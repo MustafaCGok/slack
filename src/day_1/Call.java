57621460b01c3222bf773635d12c75c18237a375
MustafaCGok <muccemil@gmail.com>
2020-05-13 18:44:18 +0200

    add only Call.java class

---
+++ src/day_1/Call.java
@@ -0,0 +1,15 @@
+package day_1;
+
+import com.sun.tools.javac.Main;
+
+public class Call {
+
+    // users afe able to call
+
+        //  git push --set-upstream origin <branchName>
+
+    }
+
+
+
+
