c7b3be3e62e322eed36817a839aead7c13e7b804
MustafaCGok <muccemil@gmail.com>
2020-05-13 16:58:24 +0200

    add CreateChannel.java

---
+++ src/day_2/CreateChannel.java
@@ -0,0 +1,13 @@
+package day_2;
+
+public class CreateChannel {
+    public static void main(String[] args) {
+
+        System.out.println("Useris able to create channels");
+        int x = 0;
+        if (x == 0) {
+            System.out.println("equal");
+        }
+
+    }
+}
