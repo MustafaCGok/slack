75acc4c7f63186ed5ae798ad10ff79abd26d316c
MustafaCGok <muccemil@gmail.com>
2020-05-13 16:36:56 +0200

    update Login.java

--- src/day_1/Login.java
+++ src/day_1/Login.java
@@ -7,7 +7,7 @@ public class Login {
 
 
         System.out.println("Login success");
-        // this line written in Intellij is the beginning
+        // this line written in Intellij is NOT the beginning
         
         
         // This part is written in Github and then code is pulled to the Intellij
