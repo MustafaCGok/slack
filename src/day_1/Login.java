927c31335d5877c50668d07fb71de3d764e7167d
MustafaCGok <muccemil@gmail.com>
2020-05-13 16:34:57 +0200

    update Login.java

--- src/day_1/Login.java
+++ src/day_1/Login.java
@@ -1,13 +1,13 @@
 package day_1;
 
-public class loginInSlack {
+public class Login {
 
 
     public static void main(String[] args) {
 
 
         System.out.println("Login success");
-
+        // this line written in Intellij is the beginning
         
         
         // This part is written in Github and then code is pulled to the Intellij
